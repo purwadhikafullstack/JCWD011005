aca5e9f29996250be9e7548d1cc70e1cfecac866
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-10 00:40:15 +0700

    FE-INPA-v2 Added components for Input Password custom component

--- projects/client/src/components/input/InputPassword.js
+++ projects/client/src/components/input/InputPassword.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
 
-const InputPassword = () => {
+const InputPassword = (props) => {
   return (
-    <div>InputPassword</div>
+    <InputGroup size='md'>
+      <Input
+        name={props.name}
+        pr='4.5rem'
+        type={props.show ? 'text' : 'password'}
+        placeholder={props.isConfirm? 'Konfirmasi kata sandi' : 'Kata sandi'}
+        bgColor="white" 
+        borderColor={"grey"} 
+        color={"black"}
+        value={props.value}
+        onChange={props.onChange}
+      />
+      <InputRightElement width='4.5rem'>
+        <Button h='1.75rem' size='sm' onClick={props.handleClick} colorScheme={props.show? "red" : "gray"} >
+          {props.show ? 'Hide' : 'Show'}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
   )
 }
 
