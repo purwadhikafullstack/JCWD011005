abb1398af23c4b87dc5ba6672c4f9d3bfcaaa402
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-13 01:26:43 +0700

    FE-ROUT-v1 Replaced current element with Router (BrowserRouter), Routes, & Route + Added user register route

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -2,25 +2,32 @@ import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import UserRegisterPage from "./pages/guest/UserRegisterPage";
 
 function App() {
-  const [message, setMessage] = useState("");
+  // const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      );
-      setMessage(data?.message || "");
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { data } = await axios.get(
+  //       `${process.env.REACT_APP_API_BASE_URL}/greetings`
+  //     );
+  //     setMessage(data?.message || "");
+  //   })();
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {message}
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     {message}
+    //   </header>
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/user-register" element={<UserRegisterPage/> } />
+      </Routes>
+    </Router>
   );
 }
 
