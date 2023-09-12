27ca0ef8a53ee2528049c5c59b62fab19f51d5db
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-12 22:32:58 +0700

    URPA-v5 Renamed 'addUserSchema' to 'userRegisterSchema'

--- projects/client/src/pages/guest/UserRegisterPage.js
+++ projects/client/src/pages/guest/UserRegisterPage.js
@@ -6,7 +6,7 @@ import { useFormik } from 'formik'
 import * as Yup from "yup";
 
 const UserRegisterPage = () => {
-  const addUserSchema = useFormik({
+  const userRegisterSchema = useFormik({
     initialValues: {
       roleName: ""
     },
@@ -27,21 +27,21 @@ const UserRegisterPage = () => {
     }
   });
   return (
-    <form onSubmit={addUserSchema.handleSubmit}>
-      <InputWithError margin={"0"} padding={"1"} errors={addUserSchema.errors.firstName} touched={addUserSchema.touched.firstName}>
-        <Input type="text" name="firstName" placeholder='Nama Depan' bgColor="white" borderColor={"grey"} color={"black"} value={addUserSchema.values.firstName} onChange={addUserSchema.handleChange}/>
+    <form onSubmit={userRegisterSchema.handleSubmit}>
+      <InputWithError margin={"0"} padding={"1"} errors={userRegisterSchema.errors.firstName} touched={userRegisterSchema.touched.firstName}>
+        <Input type="text" name="firstName" placeholder='Nama Depan' bgColor="white" borderColor={"grey"} color={"black"} value={userRegisterSchema.values.firstName} onChange={userRegisterSchema.handleChange}/>
       </InputWithError>
-      <InputWithError margin={"0"} padding={"1"} errors={addUserSchema.errors.firstName} touched={addUserSchema.touched.lastName}>
-        <Input type="text" name="lastName" placeholder='Nama Belakang' bgColor="white" borderColor={"grey"} color={"black"} value={addUserSchema.values.lastName} onChange={addUserSchema.handleChange}/>
+      <InputWithError margin={"0"} padding={"1"} errors={userRegisterSchema.errors.firstName} touched={userRegisterSchema.touched.lastName}>
+        <Input type="text" name="lastName" placeholder='Nama Belakang' bgColor="white" borderColor={"grey"} color={"black"} value={userRegisterSchema.values.lastName} onChange={userRegisterSchema.handleChange}/>
       </InputWithError>
-      <InputWithError margin={"0"} padding={"1"} errors={addUserSchema.errors.email} touched={addUserSchema.touched.email}>
-        <Input type="text" name="email" placeholder='Nama Depan' bgColor="white" borderColor={"grey"} color={"black"} value={addUserSchema.values.email} onChange={addUserSchema.handleChange}/>
+      <InputWithError margin={"0"} padding={"1"} errors={userRegisterSchema.errors.email} touched={userRegisterSchema.touched.email}>
+        <Input type="text" name="email" placeholder='Nama Depan' bgColor="white" borderColor={"grey"} color={"black"} value={userRegisterSchema.values.email} onChange={userRegisterSchema.handleChange}/>
       </InputWithError>
-      <InputWithError margin={"0"} padding={"1"} errors={addUserSchema.errors.password} touched={addUserSchema.touched.password}>
-        <Input type="text" name="password" placeholder='Password' bgColor="white" borderColor={"grey"} color={"black"} value={addUserSchema.values.password} onChange={addUserSchema.handleChange}/>
+      <InputWithError margin={"0"} padding={"1"} errors={userRegisterSchema.errors.password} touched={userRegisterSchema.touched.password}>
+        <Input type="text" name="password" placeholder='Password' bgColor="white" borderColor={"grey"} color={"black"} value={userRegisterSchema.values.password} onChange={userRegisterSchema.handleChange}/>
       </InputWithError>
-      <InputWithError margin={"0"} padding={"1"} errors={addUserSchema.errors.phone} touched={addUserSchema.touched.phone}>
-        <Input type="text" name="phone" placeholder='Nomor Telepon' bgColor="white" borderColor={"grey"} color={"black"} value={addUserSchema.values.phone} onChange={addUserSchema.handleChange}/>
+      <InputWithError margin={"0"} padding={"1"} errors={userRegisterSchema.errors.phone} touched={userRegisterSchema.touched.phone}>
+        <Input type="text" name="phone" placeholder='Nomor Telepon' bgColor="white" borderColor={"grey"} color={"black"} value={userRegisterSchema.values.phone} onChange={userRegisterSchema.handleChange}/>
       </InputWithError>
       <Button type="submit" colorScheme={"green"} marginX="5">Mendaftar</Button>
     </form>
