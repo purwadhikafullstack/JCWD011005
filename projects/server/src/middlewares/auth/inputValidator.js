62ae4740f0446ebc55d3e595b14d3c6d493cdb09
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-13 17:54:27 +0700

    BE-IVMI-v2 Added validator logic for email, first name, password, & phone

---
+++ projects/server/src/middlewares/auth/inputValidator.js
@@ -0,0 +1,97 @@
+const emailValidator = (req, res, next) => {
+    let result = false;
+    let { email } = req.body;
+    if (email == false) return res.status(422).send("Email tidak boleh kosong!");
+
+    email = email.toLowerCase();
+
+    // dimasivonanggitama @ gmail    .com
+    // ------------------ | ------   ----------------
+    //     email name     | domain   top level domain
+    //                    |
+    //              at / separator
+
+    const atChar = email.indexOf("@");
+    if (atChar < 0) return res.status(422).send("Email harus memiliki karakter '@' !");
+
+    const domain = email.substr(atChar + 1, email.length - 1);
+    const dotCharDomain = domain.indexOf(".");
+    if (dotCharDomain < 0) return res.status(422).send("Domain email harus memiliki karakter titik (.) !");
+
+    const domainPattern = /[a-z.]/;
+    for (let i = 0; i < domain.length; i++) {
+        result = domainPattern.test(domain[i]);
+        if (result == false) return res.status(422).send("Karakter pada domain yang diperbolehkan hanya huruf dan titik (.) !");
+    }
+
+    const aliasName = email.substr(0, atChar);
+    const aliasNamePattern = /[a-z0-9._]/;
+    for (let i = 0; i < aliasName.length; i++) {
+        result = aliasNamePattern.test(aliasName[i]);
+        if (result == false) return res.status(422).send("Karakter pada nama email yang diperbolehkan hanya huruf, angka, titik (.) dan underscore (_) !");
+    }
+
+    return next();
+};
+
+const firstNameValidator = (req, res, next) => {
+    let result = false;
+    let { firstName } = req.body;
+    if (firstName == false) return res.status(422).send("Nama depan tidak boleh kosong!");
+
+    firstName = firstName.toLowerCase();
+    const pattern = /[a-zA-Z]/;
+    for (let i = 0; i < firstName.length; i++) {
+        result = pattern.test(firstName[i]);
+        if (result == false) return res.status(422).send("Hanya huruf yang diperbolehkan!");
+    }
+    return next();
+}
+
+const passwordValidator = (req, res, next) => {
+    let result = false;
+    let { password } = req.body;
+    if (password == false) return res.status(422).send("Password tidak boleh kosong!");
+    if (password.length < 6) return res.status(422).send("Password setidaknya minimal 6 karakter!");
+
+    const capitalizePattern = /[A-Z]/;
+    for (let i = 0; i < password.length; i++) {
+        result = capitalizePattern.test(password[i]);
+        if (result) break;
+    }
+    if (result === false) return res.status(422).send("Password setidaknya terdapat huruf kapital!");
+    
+    const numericPattern = /[0-9]/;
+    for (let i = 0; i < password.length; i++) {
+        result = numericPattern.test(password[i]);
+        if (result) break;
+    }
+    if (result === false) return res.status(422).send("Password setidaknya terdapat angka!");
+    
+    const specialCharPattern = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/;
+    for (let i = 0; i < password.length; i++) {
+        result = specialCharPattern.test(password[i]);
+        if (result) break;
+    }
+    if (result === false) return res.status(422).send("Password setidaknya terdapat karakter spesial/unik!");
+
+    return next();
+}
+
+const phoneValidator = (req, res, next) => {
+    let result = false;
+    let { phone } = req.body;
+    if (phone == false) return res.status(422).send("Nomor ponsel tidak boleh kosong!");
+    if (phone.length < 10 || phone.length > 13) return res.status(422).send("Nomor ponsel harus antara 10-13 digit!");
+
+    const numericPattern = /[0-9]/;
+    for (let i = 0; i < phone.length; i++) {
+        result = numericPattern.test(phone[i]);
+        if (result === false) return res.status(422).send("Nomor ponsel yang diperbolehkan hanya angka!");
+    }
+
+    if (`${phone[0]}${phone[1]}` !== '08') return res.status(422).send("Nomor ponsel harus diawali dengan '08'!");
+    return next();
+}
+
+module.exports = { usernameValidator, emailValidator, passwordValidator, phoneValidator };
