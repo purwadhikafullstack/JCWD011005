3fa55b073678094f6bddd330f649a59b6835ff1c
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-13 18:02:13 +0700

    BE-AURO-v3 Integrated first name & last name validator from middleware

--- projects/server/src/routes/authRoutes.js
+++ projects/server/src/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const { authController } = require("../controllers");
 const router = require("express").Router();
-const { usernameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middleware");
+const { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middleware");
 
 // router.post("/login", emailValidator, passwordValidator, authController.login);
-router.post("/register", usernameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authController.register);
+router.post("/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authController.register);
 // router.post("/resendRegisterToken", /*islogin, isverified, isemail, isusername, isphone*/authController.resendRegisterToken);
 // router.patch("/verification", authController.verifyEmail);
 module.exports = router;
