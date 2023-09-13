c419ca30fc67a6eec2c9a798e4489a2db106aada
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-14 05:58:43 +0700

    BE-IVMI-v5 Deleted unused comment on emailValidator function

--- projects/server/src/middlewares/auth/inputValidator.js
+++ projects/server/src/middlewares/auth/inputValidator.js
@@ -28,48 +28,11 @@ const emailValidator = (req, res, next) => {
         schema.validateSync({
             email: email
         });
-        
+
         return next();
     } catch (err) {
         return res.status(422).send(err.message);
     }
-    
-    // return next();
-    
-    // let result = false;
-    // let { email } = req.body;
-    // if (email == false) return res.status(422).send("Email tidak boleh kosong!");
-
-    // email = email.toLowerCase();
-
-    // // dimasivonanggitama @ gmail    .com
-    // // ------------------ | ------   ----------------
-    // //     email name     | domain   top level domain
-    // //                    |
-    // //              at / separator
-
-    // const atChar = email.indexOf("@");
-    // if (atChar < 0) return res.status(422).send("Email harus memiliki karakter '@' !");
-
-    // const domain = email.substr(atChar + 1, email.length - 1);
-    // const dotCharDomain = domain.indexOf(".");
-    // if (dotCharDomain < 0) return res.status(422).send("Domain email harus memiliki karakter titik (.) !");
-
-    // const domainPattern = /[a-z.]/;
-    // for (let i = 0; i < domain.length; i++) {
-    //     result = domainPattern.test(domain[i]);
-    //     if (result == false) return res.status(422).send("Karakter pada domain yang diperbolehkan hanya huruf dan titik (.) !");
-    // }
-
-    // const aliasName = email.substr(0, atChar);
-    // const aliasNamePattern = /[a-z0-9._]/;
-    // for (let i = 0; i < aliasName.length; i++) {
-    //     result = aliasNamePattern.test(aliasName[i]);
-    //     if (result == false) return res.status(422).send("Karakter pada nama email yang diperbolehkan hanya huruf, angka, titik (.) dan underscore (_) !");
-    // }
-
-    // return next();
-    
 };
 
 const firstNameValidator = (req, res, next) => {
