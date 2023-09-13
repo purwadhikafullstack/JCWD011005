3d9dec45d69877982e863dc890ed2d3f5ba11364
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-13 16:50:32 +0700

    BE-FIRE-v2 Updated firebaseConfig

--- projects/server/src/firebase.js
+++ projects/server/src/firebase.js
@@ -1,17 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXX",
-  projectId: "XXXXXXXXX",
-  storageBucket: "XXXXXXXXXXXXXXXXXX",
-  messagingSenderId: "XXXXXXXXXXXX",
-  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  apiKey: "AIzaSyCjdKPRtJ8A9rTvyJIpLkFNYU2DGFBHT_E",
+  authDomain: "property-renting.firebaseapp.com",
+  projectId: "property-renting",
+  storageBucket: "property-renting.appspot.com",
+  messagingSenderId: "620646968083",
+  appId: "1:620646968083:web:40f17dc6202594af8048e6"
 };
-
+ 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export default app;
