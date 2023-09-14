0516f475b11e5ff94ce135075a51f6cb3a36af0e
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-15 03:05:31 +0700

    BE-FIRE-v5 Replace firebaseConfig json value with env variable

--- projects/server/src/firebase.js
+++ projects/server/src/firebase.js
@@ -10,12 +10,12 @@ const { getAuth } = require("firebase/auth");
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjdKPRtJ8A9rTvyJIpLkFNYU2DGFBHT_E",
-  authDomain: "property-renting.firebaseapp.com",
-  projectId: "property-renting",
-  storageBucket: "property-renting.appspot.com",
-  messagingSenderId: "620646968083",
-  appId: "1:620646968083:web:40f17dc6202594af8048e6"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
  
 // Initialize Firebase
