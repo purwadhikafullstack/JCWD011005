7fdcfa44c3572eae0700b73ac90cf0299162452c
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-15 05:23:23 +0700

    BE-USMI-v3 Added 'user' query interface

--- projects/server/src/migrations/20230914205607-create-user-table.js
+++ projects/server/src/migrations/20230914205607-create-user-table.js
@@ -9,6 +9,53 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable('users', {
+      user_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      first_name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.BIGINT
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      birthdate: {
+        type: Sequelize.DATEONLY
+      },
+      image_profile: {
+        type: Sequelize.STRING
+      },
+      image_id_card: {
+        type: Sequelize.STRING
+      },
+      is_verified: {
+        allowNull: false,
+        defaultValue: 0,
+        type: Sequelize.BOOLEAN
+      }
+    }, {
+      createdAt: true,
+      timestamps: false,
+      updatedAt: false
+    });
   },
 
   async down (queryInterface, Sequelize) {
