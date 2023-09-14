dc3ffdfa3a12a681cbdb2661974b2dd3bbb47fda
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-15 05:27:01 +0700

    BE-LMMI-v3 Added 'login_methods' query interface

--- projects/server/src/migrations/20230914210356-create-login_method-table.js
+++ projects/server/src/migrations/20230914210356-create-login_method-table.js
@@ -9,6 +9,22 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable('login_methods', {
+      login_method_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    }, {
+      createdAt: true,
+      timestamps: false,
+      updatedAt: false
+    });
   },
 
   async down (queryInterface, Sequelize) {
