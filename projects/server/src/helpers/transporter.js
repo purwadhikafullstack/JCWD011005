e5ad98aaf1e5419c6bd53e67d5f56696694dc94b
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-18 06:38:59 +0700

    BE-TRNS-v2 Added transporter logic code

---
+++ projects/server/src/helpers/transporter.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+const path = require('path');
+require("dotenv").config({
+  path: path.resolve('../../.env'),
+});
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASS
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
+});
+
+module.exports = transporter;
