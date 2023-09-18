78c062f3b2581870504813f702fbda8702b8d900
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-18 20:41:21 +0700

    FE-VAPA-v2 Added form components for input otp and submit

---
+++ projects/client/src/pages/guest/VerifyAccountPage.js
@@ -0,0 +1,44 @@
+import { Box, Button, Input } from '@chakra-ui/react'
+import React from 'react'
+import axios from 'axios';
+import { useFormik } from 'formik'
+import * as Yup from "yup";
+import InputWithError from '../../components/input/InputWithError';
+
+const VerifyAccountPage = () => {
+  const verifyAccountSchema = useFormik({
+    initialValues: {
+      otp: "",
+    },
+    validationSchema: Yup.object({
+        otp: Yup.string()
+        .matches(/[0-9]/, "Kode OTP yang diperbolehkan hanya angka!")
+        .max(6, "Kode OTP maksimal 6 digit!")
+        .required("Kode OTP tidak boleh kosong!")
+    }),
+    onSubmit: async values => {
+      await axios.post(`${process.env.REACT_APP_API_BASE_URL}/user/verifyAccount`, {
+        otp: values.otp,
+      }).then(resp => {
+        // props.fetchData();
+      }).catch(error => {
+        console.log(error.response.data.error);
+        alert(error.response.data.message);
+      });
+    }
+  });
+  return (
+    <Box display={"flex"} justifyContent={"center"} alignItems={"center"} height={"calc(100vh)"} width={"100%"} bgColor={"lightgrey"}>
+      <Box display={"flex"} justifyContent={"center"} alignItems={"center"} width="25%" bgColor={"white"} border={"black"} borderRadius={"15"} padding={"5"} boxShadow={"md"} >
+        <form onSubmit={verifyAccountSchema.handleSubmit}>
+          <InputWithError margin={"0"} padding={"1"} errors={verifyAccountSchema.errors.otp} touched={verifyAccountSchema.touched.otp}>
+            <Input type="text" name="otp" placeholder='Kode OTP' bgColor="white" borderColor={"grey"} color={"black"} value={verifyAccountSchema.values.phone} onChange={verifyAccountSchema.handleChange}/>
+          </InputWithError>
+          <Button type="submit" colorScheme={"green"} marginX="5">Verifikasi</Button>
+        </form>
+      </Box>
+    </Box>
+  )
+}
+
+export default VerifyAccountPage
