d819822bf7d54c05f987c2a0465cf4a54a1e642b
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-19 01:06:56 +0700

    FE-MOBL-v2 Added minimal modal components

--- projects/client/src/components/modal/ModalBlank.js
+++ projects/client/src/components/modal/ModalBlank.js
@@ -1,9 +1,16 @@
+import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from '@chakra-ui/react'
 import React from 'react'
 
-const ModalBlank = () => {
-  return (
-    <div>ModalBlank</div>
-  )
+const ModalBlank = (props) => {
+    return (
+        <Modal isOpen={props.isOpen} onClose={props.onCloseX}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalCloseButton />
+                {props.children}
+            </ModalContent>
+        </Modal>
+    )
 }
 
 export default ModalBlank
