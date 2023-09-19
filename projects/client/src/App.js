822f805c84919a380ca366068c4491c767bea371
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-20 00:23:43 +0700

    FE-ROUT-v4 Added route to Success Page for Email Sent from User Register Page

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import UserRegisterPage from "./pages/guest/UserRegisterPage";
 import VerifyAccountPage from "./pages/guest/VerifyAccountPage";
+import SuccessPage from "./pages/guest/SuccessPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/user-register" element={<UserRegisterPage/> } />
         <Route path="/verifyAccount" element={<VerifyAccountPage/> } />
+        <Route path="/user-register/emailSent" element={<SuccessPage purpose="email"/> } />
       </Routes>
     </Router>
   );
