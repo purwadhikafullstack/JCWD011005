65c7219d928162f49d585b6df50857faaf7e062d
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-19 14:57:34 +0700

    BE-MIDL-v2 Called functions from Input Validator middleware

---
+++ projects/server/src/middlewares/index.js
@@ -0,0 +1,3 @@
+const { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator } = require("./auth/inputValidator");
+
+module.exports = { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator };
