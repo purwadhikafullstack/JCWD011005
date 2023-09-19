1456e1b4dd8ec00dc3fbbf5f1653a9226b629035
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-20 02:18:38 +0700

    FE-BLPA-v2 Added blank grey background components

--- projects/client/src/pages/universal/BlankPage.js
+++ projects/client/src/pages/universal/BlankPage.js
@@ -1,8 +1,32 @@
+import { Box, Divider, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const BlankPage = () => {
+const BlankPage = (props) => {
   return (
-    <div>BlankPage</div>
+    <Box
+      bgColor="lightgrey"
+      display='flex'
+      justifyContent={"center"}
+      height="calc(100vh)"
+      width='100%'
+    >
+      <Box
+        alignItems="center"
+        bgColor="whiteAlpha.500"
+        display="flex"
+        flexDirection="column"
+        justifyItems="start"
+        width="50%"
+      >
+        <Box width="100%" textAlign="center">
+          <Text textAlign="center" as="b" fontSize="6xl" marginBottom="4">Pro-Rent</Text>
+        </Box>
+        <Divider/>
+        <Box height="100%" width="100%" display="flex" justifyContent="center" alignItems="center">
+          {props.children}
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
