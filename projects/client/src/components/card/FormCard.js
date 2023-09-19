092d608f1ca43ad52a6a935364057323bce65d04
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-20 02:50:28 +0700

    FE-FOCA-v2 Added Form Card components

--- projects/client/src/components/card/FormCard.js
+++ projects/client/src/components/card/FormCard.js
@@ -1,8 +1,25 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
-const FormCard = () => {
+const FormCard = (props) => {
   return (
-    <div>FormCard</div>
+    <Box 
+      bgColor={"white"} 
+      borderRadius={"md"} 
+      boxShadow='md' 
+      flexDirection="column"
+      minHeight="30%" 
+      padding={5} 
+      textAlign={"center"} 
+      textColor={"black"} 
+      width={"50%"} 
+      
+      display="flex" 
+      alignItems={"stretch"}
+      justifyContent={"center"}
+    >
+      {props.children}
+    </Box>
   )
 }
 
