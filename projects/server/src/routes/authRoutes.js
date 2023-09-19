33ad0cf5cafa27fc5f78aad0bdf40ca37c41b5bd
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-20 03:11:49 +0700

    BE-AURO-v8 Uncomment (activated) dbVerificator from middlewares

--- projects/server/src/routes/authRoutes.js
+++ projects/server/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middlewares");
 
 // router.post("/login", emailValidator, passwordValidator, authController.login);
-router.post("/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, /*dbVerificator,*/ authController.register);
+router.post("/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authController.register);
 // router.post("/resendRegisterToken", /*islogin, isverified, isemail, isusername, isphone*/authController.resendRegisterToken);
 // router.patch("/verification", authController.verifyEmail);
 module.exports = router;
