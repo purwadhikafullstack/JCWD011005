530a055abecd1ee6ac2abf06ed10c3649d4022eb
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-20 03:12:54 +0700

    BE-MIDL-v3 Called functions from DB Validator middleware

--- projects/server/src/middlewares/index.js
+++ projects/server/src/middlewares/index.js
@@ -1,3 +1,4 @@
 const { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator } = require("./auth/inputValidator");
+const { dbVerificator } = require("./auth/dbVerificator");
 
-module.exports = { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator };
+module.exports = { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator };
