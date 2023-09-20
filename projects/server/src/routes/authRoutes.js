cbe47633417431c17a23b5207196cd2130098b1e
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-21 03:48:34 +0700

    BE-AURO-v8 Uncomment (activated) verify route + added otp validator from middlewares

--- projects/server/src/routes/authRoutes.js
+++ projects/server/src/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const { authController } = require("../controllers");
 const router = require("express").Router();
-const { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middlewares");
+const { firstNameValidator, lastNameValidator, emailValidator, otpValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middlewares");
 
 // router.post("/login", emailValidator, passwordValidator, authController.login);
 router.post("/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authController.register);
+router.patch("/user/verify", otpValidator, authController.verify);
 // router.post("/resendRegisterToken", /*islogin, isverified, isemail, isusername, isphone*/authController.resendRegisterToken);
-// router.patch("/verification", authController.verifyEmail);
 module.exports = router;
