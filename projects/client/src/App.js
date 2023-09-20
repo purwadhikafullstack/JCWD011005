604551ff41a1bab6d98dcfb095509bd108fabf38
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-20 18:15:37 +0700

    FE-ROUT-v7 Changed route of verify account

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/user-register" element={<UserRegisterPage/> } />
         <Route path="/user-register/emailSent" element={<SuccessPage purpose="email"/> } />
-        <Route path="/verifyAccount" element={<VerifyAccountPage/> } />
+        <Route path="/user/verify" element={<VerifyAccountPage/> } />
       </Routes>
     </Router>
   );
