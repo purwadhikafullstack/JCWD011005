9a5914f9997fc32a233907679abe2fec7857a229
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-20 19:04:23 +0700

    BE-MIDL-v4 Integrated otpValidator() from Input Validator

--- projects/server/src/middlewares/index.js
+++ projects/server/src/middlewares/index.js
@@ -1,4 +1,4 @@
-const { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator } = require("./auth/inputValidator");
+const { firstNameValidator, lastNameValidator, emailValidator, otpValidator, passwordValidator, phoneValidator } = require("./auth/inputValidator");
 const { dbVerificator } = require("./auth/dbVerificator");
 
-module.exports = { firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator };
+module.exports = { firstNameValidator, lastNameValidator, emailValidator, otpValidator, passwordValidator, phoneValidator, dbVerificator };
