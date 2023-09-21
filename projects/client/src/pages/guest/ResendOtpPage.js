39e1cb7bbd4ede7647c6c67b1cb20c83fa14e31d
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-21 20:03:08 +0700

    FE-ROPA-v2 Added Email Form custom components

--- projects/client/src/pages/guest/ResendOtpPage.js
+++ projects/client/src/pages/guest/ResendOtpPage.js
@@ -1,9 +1,56 @@
 import React from 'react'
+import BlankPage from './BlankPage'
+import InputWithError from '../components/input/InputWithError'
+import { useFormik } from 'formik';
+import * as Yup from "yup";
+import axios from 'axios';
+import { Box, Button, Input, Text } from '@chakra-ui/react';
+import { TbLockQuestion } from 'react-icons/tb'
+import FormCard from '../components/card/FormCard';
 
 const ResendOtpPage = () => {
-  return (
-    <div>ResendOtpPage</div>
-  )
+    const forgotPasswordSchema = useFormik({
+        initialValues: {
+            email: "",
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+              .required("Email tidak boleh kosong!")
+              .email("Format email tidak benar!")
+        }),
+        onSubmit: async values => {
+            // alert(JSON.stringify(values, null, 2));
+  
+            // const axios = require("axios");
+            await axios.post("https://minpro-blog.purwadhikabootcamp.com/api/auth/login", {
+                email: values.email,
+            }).then(resp => {
+                alert(`[resp.data]: ${resp.data}`);
+            }).catch(error => {
+                alert(`[error.response.data.err] ${error.response.data.err}`);
+            });
+            alert("Done");
+        }
+    });
+    return (
+      <BlankPage>
+        <FormCard>
+          <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+            <TbLockQuestion size={70}/>
+          </Box>
+          <Text as="b" fontSize="2xl">Kehilangan kata sandi?</Text>
+          <Text>Kami akan membantu dalam memulihkan akun anda</Text>
+          <form onSubmit={forgotPasswordSchema.handleSubmit}>
+            <InputWithError errors={forgotPasswordSchema.errors.email} touched={forgotPasswordSchema.touched.email}>
+              <Input type="text" name="email" placeholder='Email' bgColor="white" borderColor={"grey"} color={"black"} value={forgotPasswordSchema.values.email} onChange={forgotPasswordSchema.handleChange}/>
+            </InputWithError>
+            <Box display={"flex"}>
+              <Button type="submit" colorScheme={"green"} flex={1} marginX="5">Kirim</Button>
+            </Box>
+          </form>
+        </FormCard>
+      </BlankPage>
+    )
 }
 
 export default ResendOtpPage
