a0b2d49ce1d101963bcde938793fe60707e1465e
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-21 20:22:13 +0700

    FE-SEPA-v9 Added error modal when submit failed

--- projects/client/src/pages/guest/SubmitEmailPage.js
+++ projects/client/src/pages/guest/SubmitEmailPage.js
@@ -4,12 +4,23 @@ import InputWithError from '../components/input/InputWithError'
 import { useFormik } from 'formik';
 import * as Yup from "yup";
 import axios from 'axios';
-import { Box, Button, Input, Text } from '@chakra-ui/react';
-import { TbLockQuestion } from 'react-icons/tb'
+import { Box, Button, Input, Text, useDisclosure } from '@chakra-ui/react';
+import { TbAlertTriangle, TbLockQuestion } from 'react-icons/tb'
 import FormCard from '../components/card/FormCard';
+import ModalRegular from '../../components/modal/ModalRegular';
 
 const SubmitEmailPage = () => {
+  const [errorStatus, setErrorStatus] = useState("");
+  const [errorStatusText, setErrorStatusText] = useState("");
+  const [errorData, setErrorData] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const modalAlertTitle = <Box display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
+    <TbAlertTriangle size={70}/>
+    <Text as={"b"} fontSize="2xl">Kesalahan</Text>
+  </Box>;
+
   const submitEmailSchema = useFormik({
     initialValues: {
       email: "",
@@ -26,9 +37,12 @@ const SubmitEmailPage = () => {
       }).then(resp => {
         setIsLoading(false);
         alert(`[resp.data]: ${resp.data}`);
-      }).catch(error => {
+      }).catch(err => {
         setIsLoading(false);
-        alert(`[error.response.data.err] ${error.response.data.err}`);
+        setErrorStatus(err.response.status);
+        setErrorStatusText(err.response.statusText);
+        (typeof err.response.data === 'string')? setErrorData(err.response.data) : setErrorData(err.response.data.message);
+        onOpen();
       });
       alert("Done");
     }
@@ -49,6 +63,13 @@ const SubmitEmailPage = () => {
             <Button type="submit" isLoading={isLoading} colorScheme={"green"} flex={1} marginX="5">Kirim</Button>
           </Box>
         </form>
+
+        <ModalRegular isLoading={isLoading} isOpen={isOpen} onCloseX={onClose} onSubmit={onClose} primaryButton="OK" primaryButtonColor="green" title={modalAlertTitle}>
+          <Box display="flex" flexDirection="column" justifyContent="center">
+            <Text as="b" fontSize="lg">{errorData}</Text>
+            <Text>({errorStatus} {errorStatusText})</Text>
+          </Box>
+        </ModalRegular>
       </FormCard>
     </BlankPage>
   )
