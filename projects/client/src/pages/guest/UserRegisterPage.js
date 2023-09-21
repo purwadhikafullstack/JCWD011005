a4a098d18360183fe1742386c4d1c5500df676f1
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-21 21:15:58 +0700

    FE-URPA-v22 Changed HTTP request path separator to '/'

--- projects/client/src/pages/guest/UserRegisterPage.js
+++ projects/client/src/pages/guest/UserRegisterPage.js
@@ -64,7 +64,7 @@ const UserRegisterPage = () => {
         phone: values.phone,
       }).then(resp => {
         setIsLoading(false);
-        navigate('/user-register/emailSent');
+        navigate('/user/register/emailSent');
       }).catch(err => {
         console.log(err.response);
         setIsLoading(false);
