dfc1abfd4862ebe71e6a50310b514baf2d4cce50
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-21 21:21:38 +0700

    FE-ROUT-v8 Changed routes path separator to '/'

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/user-register" element={<UserRegisterPage/> } />
-        <Route path="/user-register/emailSent" element={<SuccessPage purpose="email"/> } />
+        <Route path="/user/register" element={<UserRegisterPage/> } />
+        <Route path="/user/register/emailSent" element={<SuccessPage purpose="email"/> } />
         <Route path="/user/verify" element={<VerifyAccountPage/> } />
       </Routes>
     </Router>
