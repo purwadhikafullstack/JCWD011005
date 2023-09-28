4b281a1be6f2809f85415f433a7e595b1791fc6b
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-09-28 23:10:06 +0700

    BE-LMMO-v3 Added associate hasOne users table

--- projects/server/src/models/loginMethodModel.js
+++ projects/server/src/models/loginMethodModel.js
@@ -2,19 +2,23 @@ module.exports = (sequelize, Sequelize) => {
     const LoginMethod = sequelize.define('login_methods', {
         login_method_id: {
             allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            name: {
-                allowNull: false,
-                type: Sequelize.STRING
-            },
-        }, {
-            createdAt: false,
-            timestamps: false,
-            updatedAt: false
-        });
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        name: {
+            allowNull: false,
+            type: Sequelize.STRING
+        },
+    }, {
+        createdAt: false,
+        timestamps: false,
+        updatedAt: false
+    });
+
+    LoginMethod.associate = (models) => {
+        LoginMethod.hasOne(models.users, { foreignKey: 'user_id' });
+    };
 
     return LoginMethod;
 }
