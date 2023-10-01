0506e9f1a9f25ae528a200522885d6f44cb2e946
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-01 07:32:29 +0700

    BE-AURO-v9 Deleted all routes besides register route

--- projects/server/src/routes/authRoutes.js
+++ projects/server/src/routes/authRoutes.js
@@ -2,8 +2,5 @@ const { authController } = require("../controllers");
 const router = require("express").Router();
 const { firstNameValidator, lastNameValidator, emailValidator, otpValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middlewares");
 
-// router.post("/login", emailValidator, passwordValidator, authController.login);
 router.post("/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authController.register);
-router.patch("/user/verify", otpValidator, authController.verify);
-// router.post("/resendRegisterToken", /*islogin, isverified, isemail, isusername, isphone*/authController.resendRegisterToken);
 module.exports = router;
