ca533458ece93000afde1241cc8d65c49a16cf53
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-02 01:09:21 +0700

    FE-FOCA-v4 Deleted curly bracket on properties to only quotation mark

--- projects/client/src/components/card/FormCard.js
+++ projects/client/src/components/card/FormCard.js
@@ -4,20 +4,20 @@ import React from 'react'
 const FormCard = (props) => {
   return (
     <Box 
-      bgColor={"white"} 
-      borderRadius={"md"} 
-      boxShadow='md' 
+      bgColor="white" 
+      borderRadius="md" 
+      boxShadow="md" 
       flexDirection="column"
       gap={props.gap}
       minHeight="30%" 
-      padding={5} 
-      textAlign={"center"} 
-      textColor={"black"} 
-      width={"50%"} 
+      padding="5" 
+      textAlign="center" 
+      textColor="black" 
+      width="50%" 
       
       display="flex" 
-      alignItems={"stretch"}
-      justifyContent={"center"}
+      alignItems="stretch"
+      justifyContent="center"
     >
       {props.children}
     </Box>
