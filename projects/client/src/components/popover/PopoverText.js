d044cb81530868b5615870ad01f6eb45b14886eb
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-02 01:05:32 +0700

    FE-POTE-v2 Added preset popover components

--- projects/client/src/components/popover/PopoverText.js
+++ projects/client/src/components/popover/PopoverText.js
@@ -1,8 +1,17 @@
+import { Popover, PopoverArrow, PopoverBody, PopoverContent, PopoverTrigger } from '@chakra-ui/react'
 import React from 'react'
 
-const PopoverText = () => {
+const PopoverText = (props) => {
   return (
-    <div>PopoverText</div>
+    <Popover size="lg" trigger="hover">
+      <PopoverTrigger>
+        {props.children}
+      </PopoverTrigger>
+      <PopoverContent bg={props.bgColor} color={props.textColor} borderColor={props.borderColor} width="auto">
+        <PopoverArrow  bg={props.bgColor}/>
+        <PopoverBody>{props.text}</PopoverBody>
+      </PopoverContent>
+    </Popover>
   )
 }
 
