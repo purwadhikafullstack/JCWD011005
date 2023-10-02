054afc36c762d15d0fc78e9e4425b788f6cf2ab0
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-02 09:36:51 +0700

    FE-ROUT-v12 Added route for landing page

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -4,11 +4,13 @@ import UserRegisterPage from "./pages/guest/UserRegisterPage";
 import VerifyAccountPage from "./pages/guest/VerifyAccountPage";
 import SuccessPage from "./pages/universal/SuccessPage";
 import SubmitEmailPage from "./pages/guest/SubmitEmailPage";
+import LandingPage from "./pages/universal/LandingPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LandingPage/> } />
         <Route path="/user/forgotPassword" element={<SubmitEmailPage purpose="password"/> } />
         <Route path="/user/register" element={<UserRegisterPage/> } />
         <Route path="/user/register/emailSent" element={<SuccessPage purpose="email"/> } />
