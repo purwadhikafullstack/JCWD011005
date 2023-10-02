a0f3428384416c2ea4e6dc57e64f523646ada39b
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-03 06:19:13 +0700

    PCMI-v1 Generated Property Category migration table

---
+++ projects/server/src/migrations/20231002040121-create-property-categories-table.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('property_categories', {
+      property_category_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    }, {
+      createdAt: false,
+      timestamps: false,
+      updatedAt: false
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
