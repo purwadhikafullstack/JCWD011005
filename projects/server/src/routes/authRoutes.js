050f1a463fb3c940c0808169e03794c4588c5417
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-02 09:32:38 +0700

    BE-AURO-v10 Added route for Google OAuth

--- projects/server/src/routes/authRoutes.js
+++ projects/server/src/routes/authRoutes.js
@@ -3,4 +3,5 @@ const router = require("express").Router();
 const { firstNameValidator, lastNameValidator, emailValidator, otpValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middlewares");
 
 router.post("/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authController.register);
+router.post("/auth/googleOAuth", authController.googleOAuth);
 module.exports = router;
