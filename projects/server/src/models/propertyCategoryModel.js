85fb26bb52bf8014c1c10a42ac498f77b442d3fe
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-03 11:52:57 +0700

    BE-PCMO-v4 Changed property_category table define to class

--- projects/server/src/models/propertyCategoryModel.js
+++ projects/server/src/models/propertyCategoryModel.js
@@ -1,5 +1,26 @@
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-    const PropertyCategory = sequelize.define('property_categories', {
+//     const PropertyCategory = sequelize.define('property_category', {
+//       property_category_id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: DataTypes.INTEGER
+//       },
+//       name: {
+//         allowNull: false,
+//         type: DataTypes.STRING
+//       },
+//     }, {
+//       createdAt: false,
+//       timestamps: false,
+//       updatedAt: false
+//     });
+
+//     return PropertyCategory;
+class PropertyCategory extends Model {}
+  PropertyCategory.init(
+  {
       property_category_id: {
         allowNull: false,
         autoIncrement: true,
@@ -11,10 +32,32 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING
       },
     }, {
-      createdAt: false,
-      timestamps: false,
-      updatedAt: false
+      // createdAt: false,
+      // timestamps: false,
+      // updatedAt: false
+      sequelize, modelName: 'property_category'
     });
-
-    return PropertyCategory;
+    return PropertyCategory
 }
+
+// 'use strict'
+// const { Model } = require('sequelize')
+// module.exports = (sequelize, DataTypes) => {
+//   class Cart_Item extends Model {
+//     static associate(models) {
+//       this.belongsTo(models.Product, { foreignKey: 'productId' })
+//       this.belongsTo(models.Cart, { foreignKey: 'cartId' })
+//     }
+//   }
+
+//   Cart_Item.init(
+//     {
+//       cartId: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//       },
+//       productId: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//       },
+//       qu
