bc8b29f276c230f0a8135d4e0f2b077d0cf5fe9c
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-03 11:53:56 +0700

    BE-PCCO-v2 Added findAll() sequelize for getCategories method

--- projects/server/src/controllers/propertyControllers.js
+++ projects/server/src/controllers/propertyControllers.js
@@ -1,13 +1,19 @@
+// const { user, PropertyCategory } = require("../models");
+const db = require("../models");
+
 const PropertyController = {
     getCategories: async (req, res) => {
         try {
-
+            const result = await db.property_category.findAll();
+            console.log(result);
+            console.log("All categories:", JSON.stringify(result, null, 2));
+            
             return res.status(200).json({
                 message: 'Data kategori properti berhasil diambil',
-                data: result,
-                token
+                data: result
             });
         } catch (err) {
+            console.log("err.message: "+err.message)
             return res.status(503).json({
                 message: 'Mohon maaf, sedang ada pemeliharaan layanan saat ini. Silakan coba lagi nanti.',
                 error: err.message
