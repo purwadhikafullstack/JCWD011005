d11f2422745ddcd78fc090ac22905279cfc69f41
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-04 18:37:19 +0700

    Added route for getCategories

--- projects/server/src/routes/propertyRoutes.js
+++ projects/server/src/routes/propertyRoutes.js
@@ -1,2 +1,5 @@
 const router = require("express").Router();
+const { propertyController } = require("../controllers");
+
+router.post("/property/categories", propertyController.getCategories);
 module.exports = router;
