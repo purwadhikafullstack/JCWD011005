5fa83b5b61adf4be2acef592cb601ba40dfce04e
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-04 15:17:13 +0700

    FE-MORE-v2 Added Modal Regular custom components

--- projects/client/src/components/modal/ModalRegular.js
+++ projects/client/src/components/modal/ModalRegular.js
@@ -1,9 +1,26 @@
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react'
 import React from 'react'
 
-const ModalRegular = () => {
-  return (
-    <div>ModalRegular</div>
-  )
+const ModalRegular = (props) => {
+    let primaryButtonText = "";
+    (props.primaryButton)? primaryButtonText = props.primaryButton : primaryButtonText = "Button";
+    return (
+        <Modal isOpen={props.isOpen} onClose={props.onCloseX}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>{props.title}</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody textAlign={"center"}>{props.children}</ModalBody>
+                <ModalFooter>
+                    {
+                        (props.secondaryButton)? <Button variant='ghost' marginRight={5} onClick={props.onClickSecondaryButton}>{props.secondaryButton}</Button>
+                        : <></>
+                    }
+                    <Button type="submit" colorScheme={props.primaryButtonColor} mr={3} onClick={props.onSubmit} isLoading={props.isLoading}>{primaryButtonText}</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
 }
 
 export default ModalRegular
