c931fe6b7b40f3baced63589dcb740294bae1b55
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-04 17:52:54 +0700

    FE-LAPA-v3 Updated landing page components

--- projects/client/src/pages/universal/LandingPage.js
+++ projects/client/src/pages/universal/LandingPage.js
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box, Button, Image, Input, Text, useDisclosure } from '@chakra-ui/react'
+import axios from 'axios';
+import ModalRegular from '../../components/modal/ModalRegular';
+import { TbAlertTriangle } from 'react-icons/tb';
 
 const LandingPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [categories, setCategories] = useState([]);
+    
+  const [errorStatus, setErrorStatus] = useState("");
+  const [errorStatusText, setErrorStatusText] = useState("");
+  const [errorData, setErrorData] = useState("");
+
+  const modalAlertTitle = <Box display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
+    <TbAlertTriangle size={70}/>
+    <Text as={"b"} fontSize="2xl">Kesalahan</Text>
+  </Box>;
+
+  const fetchData = async () => {
+    await axios.post(`${process.env.REACT_APP_API_BASE_URL}/property/categories`).then(resp => {
+      // setIsLoading(false);
+      setCategories(resp);
+      console.log(resp);
+    }).catch(err => {
+      console.log(err.response);
+      // setIsLoading(false);
+      setErrorStatus(err.response.status);
+      setErrorStatusText(err.response.statusText);
+      (typeof err.response.data === 'string')? setErrorData(err.response.data) : setErrorData(err.response.data.message);
+      onOpen();
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+}, []);
   return (
-    <div>This is LandingPage</div>
+    <Box>
+      <Box maxHeight="500px" width="100%" border="1px">
+        <Image src={`${process.env.REACT_APP_API_BASE_URL}/heros/header.jpg`} alt='Header Image' fallbackSrc="https://via.placeholder.com/1000x500"/>
+      </Box>
+      <Box padding="5">
+        <Box display="flex" flexDirection="row" backgroundColor="white" borderRadius="15" gap="5" padding="5">
+          <Input type='text' placeholder='Kota atau penginapan' backgroundColor="white" border="1px"/>
+          <Input type='text' placeholder='Check In' backgroundColor="white" border="1px"/>
+          <Input type='text' placeholder='Check Out' backgroundColor="white" border="1px"/>
+          <Input type='text' placeholder='Tipe kamar' backgroundColor="white" border="1px"/>
+          <Button type="submit">Cari</Button>
+        </Box>
+        <Text>Kategori</Text>
+        <Box display="flex" flexDirection="row" gap="5">
+          {
+            (categories)? categories.map((item, index) => (
+              <Box display="flex" flexDirection="row" id={index}>
+                <Image boxSize="100px" src={`${process.env.REACT_APP_API_BASE_URL}/categories/${item.name}.jpg`} alt={`${item.name} Image`}/>
+                <Text>${item.name}</Text>
+              </Box>
+            ))
+            : "This is Landing Page"
+          }
+        </Box>
+      </Box>
+        
+      <ModalRegular isOpen={isOpen} onCloseX={onClose} onSubmit={onClose} primaryButton="OK" primaryButtonColor="green" title={modalAlertTitle}>
+        <Box display="flex" flexDirection="column" justifyContent="center">
+          <Text as="b" fontSize="lg">{errorData}</Text>
+          <Text>({errorStatus} {errorStatusText})</Text>
+        </Box>
+      </ModalRegular>
+    </Box>
   )
 }
 
