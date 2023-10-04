a6212afd3c721c6473f0cd082f8b2a846c4bd912
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-04 10:21:51 +0700

    BE-CONF-v3 Added adaptor logic to read json config on js file

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -1,9 +1,14 @@
-{
+const path = require('path');
+require("dotenv").config({
+  path: path.resolve(__dirname, '../../.env')
+});
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
