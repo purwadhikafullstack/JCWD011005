446d5106143617c9d77c97e5eb9a05c9953f024e
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-11 06:00:35 +0700

    FE-HEAD-v2 Added Header components

--- projects/client/src/components/section/Header.js
+++ projects/client/src/components/section/Header.js
@@ -1,9 +1,14 @@
+import { Box, Button, Text} from '@chakra-ui/react'
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    const bgColor = "#1E91B6";
+    return (
+        <Box bgColor={bgColor} display="flex" flexDirection="row" alignItems="center" justifyContent="space-between" paddingX="75" paddingY="30">
+            <Text as="b" fontSize="5xl">Pro-Rent</Text>
+            <Button size="lg" colorScheme='gray' color={bgColor}>Login</Button>
+        </Box>
+    )
 }
 
 export default Header
