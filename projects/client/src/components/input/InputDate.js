cc3e543b3b0bca5a39317e9658b55121054199ae
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-10 09:56:07 +0700

    FE-INDA-v2 Added Input Date custom components

--- projects/client/src/components/input/InputDate.js
+++ projects/client/src/components/input/InputDate.js
@@ -1,9 +1,19 @@
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import React from 'react'
 
-const InputDate = () => {
-  return (
-    <div>InputDate</div>
-  )
+const InputDate = (props) => {
+    const type = (props.value)? "date" : ((props.type)? "date" : "text");
+    return (
+        <InputGroup>
+            {
+                (props.icon)? <InputLeftElement pointerEvents='none' color="black" fontSize="xl" height={props.height}>
+                    {props.icon}
+                </InputLeftElement>
+                : <></>
+            }
+            <Input type={type} name={props.name} placeholder={props.placeholder} bgColor="white" border="1px" borderColor="grey" color="black" height={props.height} width={props.width} value={props.value} onChange={props.onChange} onMouseOver={props.onMouseOver} onMouseOut={props.onMouseOut}/>
+        </InputGroup>
+    )
 }
 
 export default InputDate
