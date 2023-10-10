7906ef9865d45b46822fdbbd82ec620646b2aa2f
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-10 09:58:46 +0700

    FE-DRDM-v2 Added Dropdown Menu custom components

--- projects/client/src/components/input/DropdownMenu.js
+++ projects/client/src/components/input/DropdownMenu.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { TbChevronDown } from 'react-icons/tb'
 
-const DropdownMenu = () => {
+const DropdownMenu = (props) => {
+  const placeholder = (props.placeholder)? props.placeholder : "Button";
+  const [selectedItem, setSelectedItem] = useState(placeholder);
   return (
-    <div>DropdownMenu</div>
+    <Menu onChange={props.onChange}>
+      <MenuButton as={Button} bgColor="white" border="1px" borderColor="grey" height={props.buttonHeight} leftIcon={props.icon} rightIcon={<TbChevronDown/>}>
+        {selectedItem}
+      </MenuButton>
+      <MenuList color="black">
+        {
+          (props.value)? props.value.map((item, index) => (
+            <MenuItem value={item.value} onClick={() => setSelectedItem(item.display)}>{item.display}</MenuItem>
+          ))
+          : <MenuItem>This is menu item</MenuItem>
+        }
+        </MenuList>
+    </Menu>
   )
 }
 
