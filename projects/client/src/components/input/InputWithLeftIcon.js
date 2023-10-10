2f548e2a94e33017112f952151931908ad3550e8
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-10 09:57:27 +0700

    FE-INLI-v2 Added Input With Left Icon custom components

--- projects/client/src/components/input/InputWithLeftIcon.js
+++ projects/client/src/components/input/InputWithLeftIcon.js
@@ -1,9 +1,15 @@
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import React from 'react'
 
-const InputWithLeftIcon = () => {
-  return (
-    <div>InputWithLeftIcon</div>
-  )
+const InputWithLeftIcon = (props) => {
+    return (
+        <InputGroup>
+            <InputLeftElement pointerEvents='none' color="black" fontSize="xl" height={props.height}>
+                {props.icon}
+            </InputLeftElement>
+            <Input type='text' name={props.name} placeholder={props.placeholder} bgColor="white" border="1px" borderColor="grey" color="black" height={props.height} width={props.width} value={props.value} onChange={props.onChange}/>
+        </InputGroup>
+    )
 }
 
 export default InputWithLeftIcon
