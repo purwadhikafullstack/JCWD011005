49e227b95604619b37c3ab5ea14d191d87b3c954
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-15 05:46:29 +0700

    FE-INDX-v2 Wrapped Provider component from react-redux with store property

--- projects/client/src/index.js
+++ projects/client/src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ChakraProvider>
+  <Provider store={store}>
+    <ChakraProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChakraProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
