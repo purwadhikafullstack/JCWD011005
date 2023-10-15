2c56c14a060516aff5d3623049109694da390f1d
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-15 07:42:56 +0700

    BE-SRVC-v5 Integrated propertyValidateAuth to index.js of services

--- projects/server/src/services/index.js
+++ projects/server/src/services/index.js
@@ -1,8 +1,10 @@
+const propertyValidateAuth = require("./propertyValidateAuth");
 const tenantValidateAuth = require("./tenantValidateAuth");
 const userValidateAuth = require("./userValidateAuth");
 const utils = require("./utils");
 
 module.exports = {
+    propertyValidateAuth,
     tenantValidateAuth,
     userValidateAuth,
     utils
