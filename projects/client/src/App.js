b46a51ca75fb159b2c41d0e41050061c5ea2a28a
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-16 16:12:39 +0700

    FE-ROUT-v6 Added route to User Login page

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/universal/LandingPage";
+import { UserLogin } from "./pages/auth/UserLogin";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
+        <Route path="/auth/user/login" element={<UserLogin/>} />
       </Routes>
     </Router>
   );
