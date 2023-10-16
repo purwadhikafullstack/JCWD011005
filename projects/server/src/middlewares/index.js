36f31d3805d1d3a2ce41c34a9952b39e1ee5613f
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-17 06:42:27 +0700

    BE-MIDL-v1 Integrated errorValidator to index.js of middlewares

--- projects/server/src/middlewares/index.js
+++ projects/server/src/middlewares/index.js
@@ -1,7 +1,9 @@
 // const { exampleNameValidator,  exampleEmailValidator, examplePasswordValidator } = require("./example/exampleValidator");
+const errorValidator = require("./errorValidator");
 
 module.exports = { 
     // exampleNameValidator, 
     // exampleEmailValidator, 
     // examplePasswordValidator, 
+    errorValidator
 };
