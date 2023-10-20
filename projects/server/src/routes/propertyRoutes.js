4ece9fc9b138357500ea99cee2c73039d63967ea
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-21 05:49:39 +0700

    Deleted unused propertyRoutes.js file

--- projects/server/src/routes/propertyRoutes.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router();
-const { propertyController } = require("../controllers");
-
-router.post("/property/categories", propertyController.getCategories);
-module.exports = router;
