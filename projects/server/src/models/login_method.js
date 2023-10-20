151da2a648a6ece5d221898ed4520d242f5f0e48
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-20 10:57:32 +0700

    BE-LMMO-v6 Changed login method detail model

--- projects/server/src/models/login_method.js
+++ projects/server/src/models/login_method.js
@@ -1,24 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
-    const LoginMethod = sequelize.define('login_methods', {
-        login_method_id: {
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-            type: DataTypes.INTEGER
-        },
-        name: {
-            allowNull: false,
-            type: DataTypes.STRING
-        },
-    }, {
-        createdAt: false,
-        timestamps: false,
-        updatedAt: false
-    });
-
+    const LoginMethod = sequelize.define('LoginMethod', {
+      login_method_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: DataTypes.STRING(255),
+    },
+    {
+      tableName: 'login_methods'
+    }, {});
+  
     LoginMethod.associate = (models) => {
-        LoginMethod.hasOne(models.users, { foreignKey: 'user_id' });
+      LoginMethod.hasOne(models.User, { foreignKey: 'user_id' });
     };
-
+  
     return LoginMethod;
-}
+  };
+  
