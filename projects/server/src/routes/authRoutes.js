f82527e79faf90e932614304700260184f2c10c1
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-20 17:12:44 +0700

    BE-AURO-v13 Changed API route for user register

--- projects/server/src/routes/authRoutes.js
+++ projects/server/src/routes/authRoutes.js
@@ -2,5 +2,5 @@ const { authUserController } = require("../controllers");
 const router = require("express").Router();
 const { firstNameValidator, lastNameValidator, emailValidator, otpValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middlewares");
 
-router.post("/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authUserController.register);
+router.post("/auth/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authUserController.register);
 module.exports = router;
