a346543899ab1359e886d07520215c7eaf370eb8
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-20 18:48:40 +0700

    FE-ROUT-v14 Deleted unused forgotPassword, resendOtp, verify token, & account verified route page

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import VerifyAccountPage from "./pages/guest/VerifyAccountPage";
 import SuccessPage from "./pages/universal/SuccessPage";
-import SubmitEmailPage from "./pages/guest/SubmitEmailPage";
 import LandingPage from "./pages/universal/LandingPage";
 import UserRegisterPage from "./pages/auth/UserRegisterPage";
 
@@ -11,12 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/> } />
-        <Route path="/auth/user/forgotPassword" element={<SubmitEmailPage purpose="password"/> } />
         <Route path="/auth/user/register" element={<UserRegisterPage/> } />
         <Route path="/auth/user/register/emailSent" element={<SuccessPage purpose="email"/> } />
-        <Route path="/auth/user/resendOtp" element={<SubmitEmailPage purpose="otp"/> } />
-        <Route path="/auth/user/verify/:token?" element={<VerifyAccountPage/> } />
-        <Route path="/auth/user/verified" element={<SuccessPage purpose="verified"/> } />
       </Routes>
     </Router>
   );
