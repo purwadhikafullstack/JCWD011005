0d2e1c8165cd1077c7a0ef3448295ee2b624fdc6
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-20 10:34:29 +0700

    BE-AURO-v12 Fixed reference path of user register

--- projects/server/src/routes/authRoutes.js
+++ projects/server/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
-const { authController } = require("../controllers");
+const { authUserController } = require("../controllers");
 const router = require("express").Router();
 const { firstNameValidator, lastNameValidator, emailValidator, otpValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middlewares");
 
-router.post("/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authController.register);
+router.post("/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authUserController.register);
 module.exports = router;
