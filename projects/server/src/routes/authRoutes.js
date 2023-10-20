3ad29588f74644d4a22901c2c39516c966f361cc
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-20 10:18:03 +0700

    BE-AURO-v11 Deleted unused googleOAuth route

--- projects/server/src/routes/authRoutes.js
+++ projects/server/src/routes/authRoutes.js
@@ -3,5 +3,4 @@ const router = require("express").Router();
 const { firstNameValidator, lastNameValidator, emailValidator, otpValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator } = require("../middlewares");
 
 router.post("/user/register", firstNameValidator, lastNameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authController.register);
-router.post("/auth/googleOAuth", authController.googleOAuth);
 module.exports = router;
