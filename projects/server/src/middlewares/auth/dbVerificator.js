8e2637c56c6d5bf994e4ee8f6dcca0eff6374384
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-20 16:10:35 +0700

    BE-DVMI-v4 Fixed user model name reference

--- projects/server/src/middlewares/auth/dbVerificator.js
+++ projects/server/src/middlewares/auth/dbVerificator.js
@@ -1,14 +1,14 @@
-const { users } = require('../../models');
+const { User } = require('../../models');
 
 const dbVerificator = async (req, res, next) => {
     const { email, phone } = req.body;
     try {
-        const emailExist = await users.findOne({
+        const emailExist = await User.findOne({
             where: { email: email },
         });
         if (emailExist) return res.status(409).json({ message: "Email sudah digunakan sebelumnya" });
         
-        const phoneExist = await users.findOne({
+        const phoneExist = await User.findOne({
             where: { phone: phone },
         });
         if (phoneExist) return res.status(409).json({ message: "Nomor telepon sudah digunakan sebelumnya" });
