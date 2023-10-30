5209263b4f067727afc3658e9c54dda550b2cff7
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-10-30 09:35:45 +0700

    FE-INPA-v1 Added InputPassword custom component with new onBlur property

---
+++ projects/client/src/components/input/InputPassword.js
@@ -0,0 +1,27 @@
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+
+const InputPassword = (props) => {
+  return (
+    <InputGroup size='md'>
+      <Input
+        name={props.name}
+        pr='4.5rem'
+        type={props.show ? 'text' : 'password'}
+        placeholder={props.isConfirm? 'Konfirmasi kata sandi' : 'Kata sandi'}
+        bgColor="white" 
+        borderColor={"grey"} 
+        color={"black"}
+        value={props.value}
+        onBlur={props.onBlur}
+        onChange={props.onChange}
+      />
+      <InputRightElement width='4.5rem'>
+        <Button h='1.75rem' size='sm' onClick={props.handleClick} colorScheme={props.show? "red" : "gray"} >
+          {props.show ? 'Hide' : 'Show'}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  )
+}
+
+export default InputPassword
